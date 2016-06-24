d8edd1eff7b0b9fb9a8208f916487a45754b263f
iLyngklip <iLyngklip@ilyngklip-2.home>
2016-06-24 20:17:08 +0200

    0.0.1 - Just beginning
    Written a little piece of the HID-USB serial to USB parser.

--- HID-USB/HID-USB.ino
+++ HID-USB/HID-USB.ino
@@ -1,9 +1,39 @@
+#define DEBUG 0
+
+#define OK_MSG  0xFF
+
+
+#include <HID-Project.h>
+#include <HID-Settings.h>
+
+#define UPDATE_INTERVAL_MS  500
+
+uint16_t timeHolder = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
 
+  Serial1.begin(2000000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(millis() - timeHolder < UPDATE_INTERVAL_MS){
+    Serial1.print(OK_MSG);
+    // Wait for data to be sent
+    while(Serial1.available() <= 0){}
+    
+    uint16_t temp = 0;
+    temp |= Serial.read() << 8;
+    temp |= Serial.read();
+    Gamepad.xAxis(temp);
+    temp = 0x00;
+  
+    temp |= Serial.read() << 8;
+    temp |= Serial.read();
+    Gamepad.yAxis(temp);
+    temp = 0x00;
+    
+  }
 }
