f6ac35ce27eb1257b0ea11f202ab7a1895925a17
iLyngklip <iLyngklip@ilyngklip-2.home>
2016-06-28 01:27:28 +0200

    0.0.8 - Getting somewhere
    Find the bug!
    Better data-checking after receiving

--- USB-MCU/USB-MCU.ino
+++ USB-MCU/USB-MCU.ino
@@ -8,11 +8,12 @@
 
 // ----------------------------------------------------
 // Debug settings -------------------------------------
-#define DEBUG 0
+#define DEBUG 1
+#define INTERNAL_BAUDRATE_DEBUG 9600
 
 // ----------------------------------------------------
 // Serial settings ------------------------------------
-#define INTERNAL_BAUDRATE 2000000
+#define INTERNAL_BAUDRATE 9600
 #define NUMBER_OF_BYTES   17
     //-----------------------------------------|
     // Messages                                |
@@ -27,9 +28,10 @@
 
 // ----------------------------------------------------
 // Includes -------------------------------------------
-#include <HID-Project.h>
-#include <HID-Settings.h>
-
+#if DEBUG == 0
+  #include <HID-Project.h>
+  #include <HID-Settings.h>
+#endif
 
 // ----------------------------------------------------
 // Global variables -----------------------------------
@@ -43,30 +45,76 @@ uint16_t timeHolder = 0;
 
 
 void setup() {
-  Serial1.begin(INTERNAL_BAUDRATE);
-  Gamepad.begin();
+  #if DEBUG
+    Serial.begin(INTERNAL_BAUDRATE_DEBUG);
+    Serial.print("Hello world!");
+    delay(2000);
+  #else
+    Serial1.begin(INTERNAL_BAUDRATE);
+  #endif
+
+  #if DEBUG == 0
+    Gamepad.begin();
+  #endif
 }// Setup
 
 void loop() {
+    #if DEBUG
+      Serial.print(OK_MSG);
+      // Wait for ALL the data to be sent
+      while(Serial.available() <= NUMBER_OF_BYTES);
+      
+      uint16_t temp = 0;
+      temp |= Serial.read() << 8;
+      Serial.print("Temp << 8: ");
+      Serial.print(temp);
+      
+      temp |= Serial.read();
+      Serial.print("Temp total: ");
+      Serial.print(temp);
+      
+      // Gamepad.xAxis(temp);
+      temp = 0x00;
+    
+      temp |= Serial.read() << 8;
+      temp |= Serial.read();
+      // Gamepad.yAxis(temp);
+      temp = 0x00;
   
-  if(millis() - timeHolder < UPDATE_INTERVAL_MS){
+
+      Serial.println();
+      Serial.println();
+    #else
+  
     Serial1.print(OK_MSG);
     
-    // Wait for data to be sent
+    // Wait for ALL the data to be sent
     while(Serial1.available() <= NUMBER_OF_BYTES);
     
     uint16_t temp = 0;
-    temp |= Serial.read() << 8;
-    temp |= Serial.read();
+    // Get the most significant byte
+    temp |= Serial1.read() << 8;    
+    // Least significant
+    temp |= Serial1.read();    
+    // Store it
     Gamepad.xAxis(temp);
+
+    // Clear temp
     temp = 0x00;
   
-    temp |= Serial.read() << 8;
-    temp |= Serial.read();
+    temp |= Serial1.read() << 8;
+    temp |= Serial1.read();
     Gamepad.yAxis(temp);
     temp = 0x00;
+
+    // End the DEBUG case
+    #endif 
+
+
+
     
-  }
+    // Now we wait for update interval
+    delay(UPDATE_INTERVAL_MS);
 }// Loop
 
 
